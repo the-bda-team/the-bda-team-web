658c66ee37147b8bc3bf7e987274d75fffbb1aa0
Johannes Kiesel <Johannes.Kiesel@gesis.org>
2026-08-05 13:10:09 +0200

    Restructured so that schemas are in _includes

---
+++ assets/js/schemas.js
@@ -0,0 +1,10 @@
+---
+---
+const SCHEMAS = {
+  base: {% include schemas/base.json %},
+  publications: {% include schemas/publications.json %},
+  people: {% include schemas/people.json %},
+  events: {% include schemas/events.json %},
+  data: {% include schemas/data.json %}
+};
+
